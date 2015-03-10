19953d05bfaa9fe0bd391064ebe9d7a17a414c41
Harsh Chawla <hchawla@localhost.localdomain>
2015-03-09 20:08:01 -0400

    Final Push
    Signed-off-by: Harsh Chawla <hchawla@localhost.localdomain>

--- OpenMP/openmp.sh
+++ OpenMP/openmp.sh
@@ -2,4 +2,4 @@
 #PBS -l nodes=1:fourcore
 #PBS -l walltime=00:01:00
 #PBS -N Tournament
-/nethome/hchawla7/CS6210-Barrier_Sync/OpenMP/Tournament 8 100
+/nethome/hchawla7/CS6210-Barrier_Sync/OpenMP/Tournament 8 10
